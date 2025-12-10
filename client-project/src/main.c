0bac8fe8f2cf8ad282f77004eb561cf310724c98
Manuel Lamanna <manuel.lam0801@gmail.com>
2025-12-10 14:26:05 +0100

    warning fix

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
 	char response_buffer[BUFFER_SIZE];
 	struct sockaddr_in clnt;
 	int len = sizeof(clnt);
-	if(recvfrom(c_socket,(void*)response_buffer,BUFFER_SIZE,0,(struct sockaddr*)&clnt,/*(socklen_t*)*/&len)<=0)
+	if(recvfrom(c_socket,(void*)response_buffer,BUFFER_SIZE,0,(struct sockaddr*)&clnt,(socklen_t*)&len)<=0)
 	{
 		printf("Errore ricezione.\n");
 		return 1;
